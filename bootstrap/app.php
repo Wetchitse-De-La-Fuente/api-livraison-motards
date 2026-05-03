0435d7d767fd76045a7f1a61c2992fb3032fff2a
Wetchitse-De-La-Fuente <linformatiquedewetchitse@gmail.com>
2026-05-03 23:38:17 +0200

    Initial commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
-        $middleware->append(HandleCors::class);
+        $middleware->prepend(HandleCors::class);
 
         $middleware->alias([
             'role' => RoleMiddleware::class,
